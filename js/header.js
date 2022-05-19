b55f4d716863ad9d08f4b5f2155b958ee008d263
LaloRGDO <95454688+LaloRGDO@users.noreply.github.com>
2022-05-19 12:28:48 -0500

    arreglando boton home

--- js/header.js
+++ js/header.js
@@ -1,17 +1,17 @@
 const login = document.querySelector(".login");
-const logo = document.querySelector(".logo");
-const logo_login = document.querySelector("#logo_login");
+// const logo = document.querySelector(".logo");
+// const logo_login = document.querySelector("#logo_login");
 
 login.addEventListener("click", ()=>{
     location.href="login.html"
 });
 
-logo.addEventListener("click", ()=>{
-    location.href="index.html"
-    console.log("index")
-});
+// logo.addEventListener("click", ()=>{
+//     location.href="index.html"
+//     console.log("index")
+// });
 
-logo_login.addEventListener("click", ()=>{
-    location.href="index.html"
-    console.log("login")
-});
+// logo_login.addEventListener("click", ()=>{
+//     location.href="index.html"
+//     console.log("login")
+// });
